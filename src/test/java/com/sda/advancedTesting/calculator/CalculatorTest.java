6deb38699fe30bacbaaa7546e306caa49273f30b
Torok Roland <torok.roland1998@gmail.com>
2023-07-09 11:06:40 +0300

    Commited the sumTest

--- src/test/java/com/sda/advancedTesting/calculator/CalculatorTest.java
+++ src/test/java/com/sda/advancedTesting/calculator/CalculatorTest.java
@@ -1,4 +1,26 @@
 package com.sda.advancedTesting.calculator;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+//recunoaste ca este o clasa pentru testare
+@ExtendWith(MockitoExtension.class)
 public class CalculatorTest {
+    @Test
+    void sumTest() {
+        // given
+        double a = 20;
+        double b = 32;
+        Calculator calc = new Calculator();
+
+        //when
+        double result = calc.sum(a,b);
+
+        //then
+        assertEquals(52,result);
+
+    }
 }

a82f517f8fb2a8075d1b0e3572d2718f90064862
Torok Roland <torok.roland1998@gmail.com>
2023-07-09 09:38:49 +0300

    Starting advanced testing

---
+++ src/main/java/com/sda/advancedTesting/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.sda.advancedTesting.calculator;
+
+public class Calculator {
+    public double sum (double noOne, double noTwo){
+        return noOne + noTwo;
+    }
+
+    public double
+
+    public int modulo(double noOne, double noTwo){
+        return noOne % noTwo;
+    }
+
+
+}

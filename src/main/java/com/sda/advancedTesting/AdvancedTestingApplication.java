d8849eafcd5fff1688a82abbfc80942a83414c7b
Torok Roland <torok.roland1998@gmail.com>
2023-07-09 09:31:26 +0300

    Starting advanced Testing

---
+++ src/main/java/com/sda/advancedTesting/AdvancedTestingApplication.java
@@ -0,0 +1,13 @@
+package com.sda.advancedTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedTestingApplication.class, args);
+	}
+
+}
